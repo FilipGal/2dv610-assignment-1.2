77853809a5e69bdeafcb5e51a42786bd682081eb
William Robertson <c.william.robertson@gmail.com>
2018-11-13 16:29:02 +0100

    save

---
+++ CardGame/CardGame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CardGame.Services
+{
+  public class CardGameService
+  {
+    public bool IsPrime(int candidate)
+    {
+      if (candidate == 1)
+    {
+        return false;
+    }
+    throw new NotImplementedException("Please create a test first");
+    }
+
+  }
+}
