add23cc809e7cfe82cce6b9a1edf91cd8f9c62b0
William Robertson <c.william.robertson@gmail.com>
2018-11-14 16:24:56 +0100

    RED

--- CardGame.Tests/CardTest.cs
+++ CardGame.Tests/CardTest.cs
@@ -1,7 +1,17 @@
+using System;
+using Xunit;
+using CardGame.model;
+
 namespace CardGame.Tests
 {
-    public class CardTest
+  public class CardTest
+  {
+    [Fact]
+    public void ShouldGetCardValue()
     {
-        
+      Card sut = new Card();
+      int expected = sut.GetCardValue();
+      Assert.InRange(expected, 2, 10);
     }
+  }
 }
--- CardGame/model/Card.cs
+++ CardGame/model/Card.cs
@@ -1,7 +1,12 @@
+using System;
+using System.Collections.Generic;
+
 namespace CardGame.model
 {
     public class Card
     {
-        
+      public int GetCardValue() {
+          return 1;
+      }
     }
 }
