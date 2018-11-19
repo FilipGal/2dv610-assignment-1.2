a7875dcb90b77abc18e4553764282c5d0485431f
William Robertson <c.william.robertson@gmail.com>
2018-11-19 15:13:37 +0100

    RED

--- CardGame.Tests/PlayerTest.cs
+++ CardGame.Tests/PlayerTest.cs
@@ -1,7 +1,21 @@
+using System;
+using Xunit;
+using CardGame.model;
+
 namespace CardGame.Tests
 {
     public class PlayerTest
     {
-        
+        [Fact]
+        public void ShouldSetPlayerName()
+        {
+          Player player = new Player();
+
+          string actual = "Daniel";
+          player.SetPlayerName();
+          string expected = player.name;
+
+          Assert.Equal(actual, expected);
+        }
     }
 }
--- CardGame/model/Player.cs
+++ CardGame/model/Player.cs
@@ -1,7 +1,14 @@
+using System;
+
 namespace CardGame.model
 {
     public class Player
     {
-        
+        public string name = "";
+
+        public void SetPlayerName()
+        {
+          throw new NotImplementedException("Not yet implemented");
+        }
     }
 }
