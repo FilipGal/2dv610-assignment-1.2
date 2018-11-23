9ba7d1267d87471740b837f6f499e3b0edf01a40
fg222ak <fg222ak@student.lnu.se>
2018-11-23 15:06:46 +0100

    RED

--- CardGame.Tests/CardGameTest.cs
+++ CardGame.Tests/CardGameTest.cs
@@ -1,15 +1,34 @@
 using Xunit;
+using Moq;
 using CardGame.model;
 
 namespace CardGame.Tests
 {
     public class CardGameTest
     {
-        private readonly Game _CardGame;
+        Game sut;
 
         public CardGameTest()
         {
-            _CardGame = new Game();
+            sut = new Game();
+        }
+
+        [Fact]
+        public void GameHasPlayerAndDealer()
+        {
+            var fakeDealer = new Mock<Dealer>();
+            var fakePlayer = new Mock<Player>();
+
+            Player player = fakePlayer.Object;
+            Dealer dealer = fakeDealer.Object;
+
+            sut.CreateGame(player, dealer);
+
+            bool playerExists = sut.player != null;
+            bool dealerExists = sut.dealer != null;
+            
+            Assert.True(playerExists);
+            Assert.True(dealerExists);
         }
     }
 }
--- CardGame/model/CardGame.cs
+++ CardGame/model/CardGame.cs
@@ -4,11 +4,20 @@ namespace CardGame.model
 {
   public class Game
   {
+
+    public Dealer dealer;
+    public Player player;
+    
     public void StartGame()
     {
       CardFactory factory = new CardFactory();
       Deck deck = new Deck();
       deck.GenerateDeck(factory);
+    }
+
+    public void CreateGame(Player p, Dealer d)
+    {
+      throw new NotImplementedException("Not yet implemented");
     }
   }
 }
